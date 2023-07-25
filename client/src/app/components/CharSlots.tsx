33e76da25acb44e06c4448de0d22ca713aa1d4fa
PradoNicolas <nicolasprado.aquicob@gmail.com>
2023-07-25 17:54:55 -0300

    planning logic logic

---
+++ client/src/app/components/CharSlots.tsx
@@ -0,0 +1,3 @@
+export default function CharSlots(){
+    return <p>Slots</p>
+}
---
+++ client/src/app/components/WrapperCharSlots.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useEffect } from 'react'
+import styles from './styles/charSlots.module.scss'
+
+export default function CharSlots({
+    children
+}: {
+    children: React.ReactNode
+}){
+
+    useEffect(() => {
+        
+    })
+
+    return <div className={styles.charSlots}>
+        childre
+    </div>
+}
