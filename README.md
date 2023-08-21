ab1e15cde089d7ca3d036d159e76710ffe193a52
PradoNicolas <nicolasprado.aquicob@gmail.com>
2023-08-21 13:32:09 -0300

    patch form overlap + image preview

--- README.md
+++
@@ -1,4 +0,0 @@
-# Real-Leveling
-Real-Leveling is a personal project, which will be based on the rpg system, bring these ideas to reality with a little creativity
-
-Made in NextJS(with intention to learn more about the framework) and Express.js to manage the back-end structure.
--- client/src/app/@login/create-char/page.tsx
+++ client/src/app/@login/create-char/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useMutation } from "react-query"
+import { useRef, useState } from "react"
 import styles from "../../styles/createchar.module.scss"
 
 export default function CreateChar({
@@ -8,6 +9,9 @@ export default function CreateChar({
 }: {
     searchParams: { [key: string]: string | string[] | undefined }
 }){
+    const imgElementRef = useRef(null)
+    const [ imgSrc, setImgSrc ] = useState<string|null>(null)
+
     const characterMutation = useMutation({
         mutationFn: (newCharacter: FormData) => {
             return fetch("http://localhost:22194/characters/improved", {
@@ -42,20 +46,46 @@ export default function CreateChar({
     }
 
     return (
-        <form onSubmit={handleSubmit} className={styles['char-form']}>
-            <div className={styles['two-corner-border']}>
-                <input type="file" name="image" id="image"/>
-                <label htmlFor="image">Image</label>
+        <div className={styles['two-corner-border']}>
+            <form onSubmit={handleSubmit} className={styles['char-form']}>                
+                <input type="file" name="image" id="image" accept="image/*" onChange={(event) => {
+                    const input = event.currentTarget.files
+                    if (input){
+                        const file = input[0]
+                        const reader = new FileReader()
+
+                        reader.onloadend = () => {
+                            setImgSrc(typeof reader.result === "string" ? reader.result : null)
+                        }
+
+                        if (file) {
+                            reader.readAsDataURL(file)
+                        }
+                    }
+                }}/>
+                <div className={styles['image-wrapper']}>
+                    <label htmlFor="image">
+                        {!imgSrc
+                            ? <span>+</span>
+                            : <img src={imgSrc} ref={imgElementRef} alt="Character image"/>
+                        }
+                    </label>
+                </div>
+
                 <input type="text" name="name" id="name"/>
                 <label htmlFor="name">Name</label>
+
                 <input type="text" name="title" id="title" value={"The weakest one"} readOnly/>
                 <label htmlFor="title">Title</label>
+
                 <input type="date" name="bornDate" id="bornDate" max={new Date().toISOString().slice(0,10)}/>
                 <label htmlFor="bornDate">Born date</label>
+
                 <textarea name="synopsis" id="synopsis" cols={30} rows={10}></textarea>
                 <label htmlFor="synopsis">Synopsis</label>
+
                 <button type="submit">Create</button>
-            </div>
-        </form>
+            </form>
+        </div>
     )
 }
